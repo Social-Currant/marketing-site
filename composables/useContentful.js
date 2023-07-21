28c1335aa5ef0df8519597ecceaac5bfdd19e1b5
Will Cheshier <47699308+wchesh24@users.noreply.github.com>
2023-07-21 13:52:55 -0700

    switch to default export

--- composables/useContentful.js
+++ composables/useContentful.js
@@ -1,4 +1,4 @@
-import { createClient } from 'contentful';
+import * as contentful from 'contentful'; // Import the entire module as 'contentful'
 
 export default function useContentful() {
   /* eslint-disable */
@@ -7,5 +7,5 @@ export default function useContentful() {
     space: config.public.ctfSpace,
     accessToken: config.public.ctfAccessToken
   }
-  return createClient(contentfulConfig);
+  return contentful.createClient(contentfulConfig); // Access the 'createClient' method from 'contentful'
 }
