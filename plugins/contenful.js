f66965c043ee7e2c1820fdc9fa71883da1debd13
Alejandro Caceres <ale@Alejandros-MacBook-Pro.local>
2024-04-19 11:45:26 -0400

    Revert to original code of contenful.js

--- plugins/contenful.js
+++ plugins/contenful.js
@@ -1,40 +1,27 @@
 import { createClient } from "contentful";
 import contentful from 'contentful'
 
-console.log("this is context", process.env.CONTEXT)
-console.log("this is netlify", process.env.NETLIFY)
-
-
 /* eslint-disable */
 export default defineNuxtPlugin((_) => {
   const config = useRuntimeConfig();
 
-  const createClientFunc = process.env.NETLIFY !== 'true' ? createClient : contentful.createClient
+  const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+  const previewCreateClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
 
   const client = createClientFunc({
     space: config.public.ctfSpace,
     accessToken: config.public.ctfAccessToken
   });
-  const previewClient = createClientFunc({
+  const previewClient = previewCreateClientFunc({
     space: config.public.ctfSpace,
     accessToken: config.public.ctfPreviewToken,
     host: 'preview.contentful.com',
   });
 
-  if (process.env.CONTEXT === "production") {
-    return {
-      provide: {
-        contentfulClient: client,
-        previewClient: previewClient
-      },
-    }
-  }
-  else {
-    return {
-      provide: {
-        contentfulClient: previewClient,
-        previewClient: previewClient
-      },
-    }
-  }
+  return {
+    provide: {
+      contentfulClient: client,
+      previewClient: previewClient
+    },
+  };
 });
