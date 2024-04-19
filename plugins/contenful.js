672bcc091ab2b6322594f04e6e8af10e39845659
Alejandro Caceres <ale@Alejandros-MacBook-Pro.local>
2024-04-19 10:23:58 -0400

    Fix named export not found

--- plugins/contenful.js
+++ plugins/contenful.js
@@ -1,4 +1,4 @@
-import { createClient } from "contentful";
+import contentful from "contentful";
 
 /* eslint-disable */
 export default defineNuxtPlugin((_) => {
@@ -9,13 +9,13 @@ export default defineNuxtPlugin((_) => {
   // https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
   if (process.env.CONTEXT === "production") {
     console.log("let's quickly check the context", process.env.CONTEXT)
-    contentfulClient = createClient({
+    contentfulClient = contentful.createClient({
       space: config.public.ctfSpace,
       accessToken: config.public.ctfAccessToken
     })
   }
   else {
-    contentfulClient = createClient({
+    contentfulClient = contentful.createClient({
       space: config.public.ctfSpace,
       accessToken: config.public.ctfPreviewToken,
       host: 'preview.contentful.com',
